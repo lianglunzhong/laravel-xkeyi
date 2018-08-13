991e041da918dadbffce2278d5504ec899585d18
vagrant <vagrant@homestead>
2018-08-13 05:43:53 +0000

    JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,24 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * The attributes that are mass assignable.
